bb1ea620f556383ee43c7cc036fd6756ac4d9b09
Alpesh Pandya <alpeshpandya@gmail.com>
2018-08-07 17:14:50 -0400

    Fix test and deploy scripts

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 ./bin/test.sh
-sbt package
 mkdir -p $AIRFLOW_HOME/dags/
-cp -R target $AIRFLOW_HOME/dags/
+mkdir -p $AIRFLOW_HOME/dags/pipeline
+cp -R target/scala-2.11/rides_by_humidity_2.11-0.1.0.jar $AIRFLOW_HOME/dags/pipeline
 cp pipeline/main/*.py $AIRFLOW_HOME/dags/
-cp -R data $AIRFLOW_HOME/dags/
+cp -R data $AIRFLOW_HOME/dags/pipeline
 cp -R pipeline/resources $AIRFLOW_HOME/dags/
--- bin/test.sh
+++ bin/test.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 sbt test
+sbt package
+cp target/scala-2.11/rides_by_humidity_2.11-0.1.0.jar pipeline
+cp -R data pipeline
 source airflowenv/bin/activate
 export PYTHONPATH=pipeline/:$PYTHONPATH
 python pipeline/test/weather_uber_rides_int_test_dag.py
